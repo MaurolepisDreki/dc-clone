1fca191e0a90a6d4d6949491607989a49b8b9cc0
MaurolepisDreki <inventerna@gmail.com>
2021-10-04 20:33:41 -0600

    DAY I: 1700->2000 (3hr)

---
+++ README.md
@@ -0,0 +1,8 @@
+A trivial clone of GNU/dc for my portfolio, created for my CS1400 course as a 
+free-range project that I "worked on this week" (in C#).
+
+This was a random draw considering that I've been focusing on my Web Framework 
+in PHP and also learning COBOL, and also that I'd never choose to work in C# 
+of my own volition; it was my first language, but (Pure) C/C++ (without 
+"modules" and such miscellaneous backwash) is prettier.
+
---
+++ main.cs
@@ -0,0 +1,128 @@
+/* A trivial dc-clone in C# */
+using System;
+using System.IO; //< Path interface
+using System.Collections.Generic; //< List and Queue containers
+
+// C uses a macro to help keep common application exit invocations clean
+//   C# should do the same...
+public enum EXIT_STATUS : int {
+	SUCCESS = 0,
+	FAILURE,
+}
+
+public class Program {
+	public static int VERSION_MAJOR = 2021;
+	public static int VERSION_MINOR = 277;
+	public static int VERSION_REVISION = 7401019;
+
+	private static List<decimal> TheStack = new List<decimal>();
+	private static Queue<string> FileQ = new Queue<string>();
+
+	private static void Help() {
+		Console.WriteLine( "Usage: {0} [OPTION] [file ...]", 
+			Path.GetFileName( 
+				System.Reflection.Assembly.GetExecutingAssembly().CodeBase
+			)
+		);
+		Console.WriteLine( "  -e, --expression=EXPR   evaluate expression" );
+		Console.WriteLine( "  -f, --file=FILE         evaluate contents of file" );
+		Console.WriteLine( "  -h, --help              display this help and exit" );
+		Console.WriteLine( "  -V, --version           output version information and exit" );
+
+		System.Environment.Exit( (int)EXIT_STATUS.SUCCESS );
+	}
+
+	private static void Version() {
+		Console.WriteLine( "{0} V{1:D4}.{2:D3} R{3:D7}",
+			Path.GetFileName(
+				System.Reflection.Assembly.GetExecutingAssembly().CodeBase
+			),
+			VERSION_MAJOR,
+			VERSION_MINOR,
+			VERSION_REVISION
+		);
+		Console.WriteLine( "  A trivial dc clone in C#" );
+		Console.WriteLine();
+		Console.WriteLine( "Authored by Maurolepis Dreki as a demonstration;" );
+		Console.WriteLine( "  NOT LICENSED FOR PERSONAL OR COMMERCIAL USE!" );
+		Console.WriteLine( "  Please use GNU/dc instead for anything unrelated to evaluating the author's coding skills.");
+
+		System.Environment.Exit( (int)EXIT_STATUS.SUCCESS );
+	}
+
+	private static void Eval( string line ) {
+		Console.WriteLine( " CRITICAL: UNIMPLIMENTED: Eval" );
+		Console.WriteLine( $"   {line}" );
+	}
+
+	private static void Read_STDIN() {
+		Console.WriteLine( " FATAL: UNIMPLIMENTED: Read_STDIN");
+		System.Environment.Exit( (int)EXIT_STATUS.SUCCESS );
+	}
+
+  public static int Main(string[] args) {
+    // Process Arguments
+		for( int ix = 0; ix < args.Length; ix++ ) {
+			// Check for Help invocation
+			if( args[ix].StartsWith( "-h" ) || args[ix].Equals( "--help") ) {
+				Help();
+			// Check for Version invocation
+			} else if( args[ix].StartsWith( "-V" ) || args[ix].Equals( "--version" ) ) {
+				Version();
+			// Check short file option
+			} else if( args[ix].StartsWith( "-f" ) ) {
+				try {
+					FileQ.Enqueue( args[ix].Length > 2 ? args[ix].Substring( 2 ) : args[++ix] );
+				} catch( IndexOutOfRangeException ) {
+					Console.WriteLine( " FATAL: expected file, saw EOL" );
+					System.Environment.Exit( (int)EXIT_STATUS.FAILURE );
+				}
+			// Check long file option
+			} else if( args[ix].StartsWith( "--file" ) ) {
+				try {
+					// This version of the program doesn't actualy care what the delimiter is, just that it is there...
+					FileQ.Enqueue( args[ix].Length > 6 ? args[ix].Substring( 7 ) : args[++ix] );
+				} catch( IndexOutOfRangeException ) {
+					Console.WriteLine( " FATAL: expected file, saw EOL" );
+					System.Environment.Exit( (int)EXIT_STATUS.FAILURE );
+				}
+			// Check short eval option
+			} else if( args[ix].StartsWith( "-e" ) ) {
+				try {
+					Eval( args[ix].Length > 2 ? args[ix].Substring( 2 ) : args[++ix] );
+				} catch( IndexOutOfRangeException ) {
+					Console.WriteLine( " FATAL: expected expression, saw EOL" );
+					System.Environment.Exit( (int)EXIT_STATUS.FAILURE );
+				}
+			// Check long eval option
+			} else if( args[ix].StartsWith( "--expression" ) ) {
+				try {
+					Eval( args[ix].Length > 12 ? args[ix].Substring( 13 ) : args[++ix] );
+				} catch( IndexOutOfRangeException ) {
+					Console.WriteLine( " FATAL: expected expression, saw EOL" );
+					System.Environment.Exit( (int)EXIT_STATUS.FAILURE );
+				}
+			// Default: read arg as file
+			} else {
+				FileQ.Enqueue( args[ix] );
+			}
+		}
+
+		// Default Mode: Read from stdin
+		if( args.Length == 0 ) {
+				Read_STDIN();
+		}
+
+		// Print Files
+		while( FileQ.Count > 0 ) {
+				Console.WriteLine( FileQ.Dequeue() );
+		}
+
+		return (int)EXIT_STATUS.SUCCESS;
+  }
+}
+
+/* CHANGE LOG:
+ *   2021.10.04 1700->2000 (3hr):
+ *     Wrote argument processor (main), Help, and Version.
+ */
